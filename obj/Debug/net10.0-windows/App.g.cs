6e98a281d8196f84562107cd50453e02e80a63bc
Inosh Matheesha <inoshmatheesha@gmail.com>
2026-06-15 13:10:35 +0530

    Update resource locators and project paths for consistency across generated files

--- obj/Debug/net10.0-windows/App.g.cs
+++ obj/Debug/net10.0-windows/App.g.cs
@@ -61,7 +61,7 @@ namespace MooDeX_New_Version_1._0 {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/MooDeX-New_Version 1.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MooDeX-New_Version 1.0;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
